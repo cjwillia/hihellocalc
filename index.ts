8c4accb978f879dac1401dfb152ff3418c8d18e7
Cory Williams <jesus@dreadlockjes.us>
2023-07-16 17:41:10 -0600

    input parser to split operators and operands

--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
 import inquirer from 'inquirer'
 
+class Calculator {
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'echo',
-        message: 'say hello >'
+}
+
+class InputParser {
+    operatorsRegExp = /[\+\-\*\/\!\%]/
+    operandsRegExp = /[\d]+/
+    parseInput(input: string) {
+        const checkValidInput = input.match(/\d|[\+\-\*\/\!\%]/g) 
+        if (!checkValidInput || checkValidInput.length != input.length) {
+            return null
+        }
+
+        const operands = input.split(this.operatorsRegExp).filter(s => s.length)
+        const operators = input.split(this.operandsRegExp).filter(s => s.length)
+        return {operands, operators}
     }
-]).then(response => {
-    console.log(response['echo'])
-})
+}
+
+const main = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'expression',
+            message: 'parse input >'
+        }
+    ]).then(response => {
+        const expression = response['expression']
+        if (expression === 'q') {
+            console.log('quitting...')
+        } else {
+            const parser = new InputParser()
+            const parsedInput = parser.parseInput(expression)
+            console.log(parsedInput !== null ? JSON.stringify(parsedInput) : 'invalid calculator input') 
+            main()
+        }
+    })
+}
+
+main()
